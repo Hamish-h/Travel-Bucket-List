b2cdfeaf760f6bb0a8ba293c176aa4a7c999c910
Hanish-H <hamish.h@outlook.com>
2018-05-07 21:50:25 +0100

    updated 'get all'

--- controllers/country_controller.rb
+++ controllers/country_controller.rb
@@ -4,3 +4,8 @@ require( 'pry-byebug' )
 require_relative( '../models/city.rb' )
 require_relative( '../models/country.rb' )
 require_relative( '../models/travel.rb' )
+
+get '/countries' do
+  @countries = Countries.all()
+  erb ( :"countries/index" )
+end
