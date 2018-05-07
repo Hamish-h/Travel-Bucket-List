5adf339e2f27645ed013c045515d27126f607e09
Hanish-H <hamish.h@outlook.com>
2018-05-07 21:26:43 +0100

    updated controller require_relative status

--- controllers/city_controller.rb
+++ controllers/city_controller.rb
@@ -1,3 +1,6 @@
 require( 'sinatra' )
 require( 'sinatra/contrib/all' )
 require( 'pry-byebug' )
+require_relative( '../models/city.rb' )
+require_relative( '../models/country.rb' )
+require_relative( '../models/travel.rb' )
