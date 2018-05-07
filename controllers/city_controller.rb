20f85e95c9c8749795d2966842a81873a57afd94
Hanish-H <hamish.h@outlook.com>
2018-05-07 21:44:03 +0100

    updated 'get all cities'

--- controllers/city_controller.rb
+++ controllers/city_controller.rb
@@ -4,3 +4,8 @@ require( 'pry-byebug' )
 require_relative( '../models/city.rb' )
 require_relative( '../models/country.rb' )
 require_relative( '../models/travel.rb' )
+
+get '/cities' do
+  @cities = Cities.all()
+  erb ( :"cities/index" )
+end
