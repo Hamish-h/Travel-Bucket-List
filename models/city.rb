4901fe8f8dfe782aa69b3624c77b562776ad5e91
Hanish-H <hamish.h@outlook.com>
2018-05-08 22:22:03 +0100

    defined edit

--- models/city.rb
+++ models/city.rb
@@ -43,6 +43,22 @@ class City
       @id = results.first()['id'].to_i
     end
 
+    def edit()
+      sql = "UPDATE cities
+      SET
+      (
+        name
+      )
+      VALUES
+      (
+        $1
+      )
+      WHERE id = $2"
+      values = [@name]
+      results = SqlRunner.run(sql, values)
+      @id = results.first()['id'].to_i
+    end
+
     def delete()
       sql = "DELETE FROM cities
       WHERE id = $1"
