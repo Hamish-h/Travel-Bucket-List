dfd981a6d3be9c1b6f82514b7cbc011b2949f991
Hanish-H <hamish.h@outlook.com>
2018-05-10 11:27:51 +0100

    added more data in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,6 +22,16 @@ country3 = Country.new({
 })
 country3.save()
 
+country4 = Country.new({
+  "name" => "Germany"
+})
+country4.save()
+
+country5 = Country.new({
+  "name" => "Trinidad"
+})
+country5.save()
+
 city1 = City.new({
   "name" => "Edinburgh",
   "review" => "Mark destinations and add sights to see ...",
@@ -43,6 +53,20 @@ city3 = City.new({
 })
 city3.save()
 
+city4 = City.new({
+  "name" => "Berlin",
+  "review" => "Mark destinations and add sights to see ...",
+  "country_id" => country4.id()
+})
+city4.save()
+
+city5 = City.new({
+  "name" => "Port of Spain",
+  "review" => "Mark destinations and add sights to see ...",
+  "country_id" => country5.id()
+})
+city5.save()
+
 travel1 = Travel.new({
   "country_id" => country1.id(),
   "city_id" => city1.id()
@@ -50,7 +74,7 @@ travel1 = Travel.new({
 travel1.save()
 
 travel2 = Travel.new({
-  "country_id" => country1.id(),
+  "country_id" => country2.id(),
   "city_id" => city2.id()
   })
 travel2.save()
@@ -60,6 +84,18 @@ travel3 = Travel.new({
   "city_id" => city3.id()
   })
 travel3.save()
+
+travel4 = Travel.new({
+  "country_id" => country4.id(),
+  "city_id" => city4.id()
+  })
+travel4.save()
+
+travel5 = Travel.new({
+  "country_id" => country5.id(),
+  "city_id" => city5.id()
+  })
+travel5.save()
 
 binding.pry
 nil
